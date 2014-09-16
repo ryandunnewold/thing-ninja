cd1a34ab38338e0d0aeb33aa8508ffd08bc8074e
Ryan Dunnewold <ryan.dunnewold@metova.com>
2014-09-16 11:17:49 -0500

    Adding list count indicators

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def thing_count_class(size)
+    case size
+      when 0..2
+        'low'
+      when 3..5
+        'medium'
+      else 
+        'high'
+    end 
+  end
 end
