202454959a87636dc4badd55ebd240463ca77813
Ryan Dunnewold <ryan.dunnewold@metova.com>
2015-04-26 22:24:29 -0500

    Removed all the things list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,10 @@
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many :things, dependent: :destroy
+  has_many :things
+
+  before_destroy :delete_things
+
+  def delete_things
+    self.things.where(finished: false).destroy_all
+  end
 end
