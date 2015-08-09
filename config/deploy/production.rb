7a95dc5d0c60b4de6470fdb32b0f310e3074cd9d
Ryan Dunnewold <rmdunnewold@gmail.com>
2015-08-09 11:58:13 -0500

    Remove production rb

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :rails_env, "production"
-set :branch, "master"
-set :domain, "107.170.111.27"
-set :deploy_to, "/apps/#{application}"
-
-server domain, :app, :web, :db, :primary => true
- 
-before "deploy:restart", "deploy:migrate"
